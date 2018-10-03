c70cc711ee14143e4e3d08d918a0cc945011b7d2
Amar Parulekar <amarparulekar2016@gmail.com>
2018-10-04 02:42:40 +0530

    Integrating Google Sign-In into app

---
+++ app/src/main/java/com/roomie/android/HomeActivity.java
@@ -0,0 +1,56 @@
+package com.roomie.android;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomeActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView textName, textEmail;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        imageView = findViewById(R.id.imageView);
+        textName = findViewById(R.id.textViewName);
+        textEmail = findViewById(R.id.textViewEmail);
+
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        Glide.with(this).load(user.getPhotoUrl()).into(imageView);
+
+        textName.setText(user.getDisplayName());
+        textEmail.setText(user.getEmail());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //if the user is not logged in
+        //opening the login activity
+        if (mAuth.getCurrentUser() == null) {
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+    }
+
+    public void signOut(View view) {
+        mAuth.signOut();
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+}
